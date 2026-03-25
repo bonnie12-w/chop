6bb5e686d0146fb5cb38e258017dbcbe0b0e10c3
bonnie12-w <bonfacekuggie@gmail.com>
2026-03-25 15:31:30 +0300

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -39,9 +39,9 @@ const Navbar = () => {
           <span className="text-2xl">🔥</span>
           <div>
             <h1 className="font-display text-lg sm:text-xl font-bold gold-gradient-text leading-tight">
-              GARDEN SQUARE BY ASHAKI
+              Tin Tin Restaurant
             </h1>
-            <p className="text-[10px] tracking-[0.3em] uppercase text-muted-foreground">GS</p>
+            <p className="text-[10px] tracking-[0.3em] uppercase text-muted-foreground">TT</p>
           </div>
         </Link>
 
